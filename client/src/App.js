0cdccf6c725c67514b604b7a7f94856de5477d9d
Viking Falk <vikingfalk@gmail.com>
2021-03-10 21:36:39 +0100

    Added access to pixabay API

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [imageURL, setImageURL] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080')
+      .then(res => res.json())
+      .then(data => {
+        console.log('data: ', data);
+        setImageURL(data.url);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
+      <img src={imageURL} />
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = 8080;
+const API_KEY = '18887902-0fdc199cd166c9d026e3f4b76';
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  try {
+    const axiosRes = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=sweden+landmark&image_type=photo&category=places&safesearch=true`);
+    res.json({ url: axiosRes.data.hits[2].largeImageURL });
+  } catch (err) {
+    console.log(err);
+    res.json({ error: err.message });
+  }
 });
 
 app.listen(port, () => {
